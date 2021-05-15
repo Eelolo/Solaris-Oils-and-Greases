a72311b49d627969198ca57899f12d42a442c6c9
Eelolo <Eelolo@yandex.ru>
2021-05-15 16:43:47 +0300

    created text model

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ class Pages(db.Model):
     name = db.Column(db.String(100))
 
     headers = db.relationship('Headers', backref='page')
+    text = db.relationship('Text', backref='page')
 
     def __init__(self, name):
         self.name = name
@@ -28,3 +29,16 @@ class Headers(db.Model):
 
     def __repr__(self):
         return f"<Header {self.tag}>"
+
+
+class Text(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100))
+
+    page_id = db.Column(db.Integer, db.ForeignKey('Pages.id'))
+
+    def __init__(self, content):
+        self.content = content
+
+    def __repr__(self):
+        return f"<Text {self.id}>"
