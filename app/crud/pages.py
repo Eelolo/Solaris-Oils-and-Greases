4c6a834327eb83b16560ed1fa97cabb0c413d874
Eelolo <Eelolo@yandex.ru>
2021-05-15 17:00:59 +0300

    Pages model crud

---
+++ app/crud/pages.py
@@ -0,0 +1,37 @@
+from app import db
+from app.models import Pages
+
+
+def create_page(name):
+    page = Pages(name)
+
+    db.session.add(page)
+    db.session.commit()
+
+
+def get_page(page_id):
+    page = Pages.query.filter_by(id=page_id).first()
+
+    return page
+
+
+def get_all_pages():
+    pages = Pages.query.all()
+
+    return pages
+
+
+def update_page(page_id, **kwargs):
+    page = get_page(page_id)
+
+    for key, value in kwargs.items():
+        setattr(page, key, value)
+
+    db.session.commit()
+
+
+def delete_page(page_id):
+    page = get_page(page_id)
+
+    db.session.delete(page)
+    db.session.commit()
