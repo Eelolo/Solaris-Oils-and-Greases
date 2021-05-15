91d6d51118ac6bba8300c7a93dc42ef82c0d6d50
Eelolo <Eelolo@yandex.ru>
2021-05-15 16:37:25 +0300

    created pages model

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from app import db
+
+
+class Pages(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f"<Page {self.name}>"
