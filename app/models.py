773f6827ac3921e8ede6146de1e5e68dd0521b35
Eelolo <Eelolo@yandex.ru>
2021-05-15 18:54:53 +0300

    admin page html

--- app/models.py
+++ app/models.py
@@ -21,7 +21,7 @@ class Headers(db.Model):
     tag = db.Column(db.String(2))
     content = db.Column(db.String(100))
 
-    page_id = db.Column(db.Integer, db.ForeignKey('Pages.id'))
+    page_id = db.Column(db.Integer, db.ForeignKey(Pages.id))
 
 
     def __init__(self, tag, content):
@@ -36,7 +36,7 @@ class Text(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(100))
 
-    page_id = db.Column(db.Integer, db.ForeignKey('Pages.id'))
+    page_id = db.Column(db.Integer, db.ForeignKey(Pages.id))
 
     def __init__(self, content):
         self.content = content
@@ -49,7 +49,7 @@ class Tables(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(10000))
 
-    page_id = db.Column(db.Integer, db.ForeignKey('Pages.id'))
+    page_id = db.Column(db.Integer, db.ForeignKey(Pages.id))
 
     def __init__(self, content):
         self.content = content
