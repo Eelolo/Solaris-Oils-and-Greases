0a09b70a8951f82f15b4e0df9130a7011132051a
Eelolo <Eelolo@yandex.ru>
2021-05-15 17:27:25 +0300

    manage.py added click commands

--- manage.py
+++ manage.py
@@ -1,10 +1,40 @@
 from app import create_app
 from app import db
+from app.models import Pages, Headers, Text, Tables
+import click
 
 
 app = create_app()
 
 
-if __name__ == '__main__':
-    print(dir(db))
+@click.group()
+def cli():
+    pass
+
+
+@click.command('create-db')
+def create_db():
+    app.app_context().push()
+    db.create_all()
+    print('All tables created')
+
+
+@click.command('drop-db')
+def drop_db():
+    app.app_context().push()
+    db.drop_all()
+    print('All tables deleted')
+
+
+@click.command('run')
+def run():
     app.run(debug=True)
+
+
+cli.add_command(create_db)
+cli.add_command(drop_db)
+cli.add_command(run)
+
+
+if __name__ == '__main__':
+    cli()
