d1d78b2362e56cc1c633b5f80cbe61a44952ccf1
Eelolo <Eelolo@yandex.ru>
2021-05-15 17:05:24 +0300

    Text model crud

---
+++ app/crud/text.py
@@ -0,0 +1,37 @@
+from app import db
+from app.models import Text
+
+
+def create_text(content, page_id):
+    text = Text(content, page_id)
+
+    db.session.add(text)
+    db.session.commit()
+
+
+def get_text(text_id):
+    text = Text.query.filter_by(id=text_id).first()
+
+    return text
+
+
+def get_all_text():
+    text = Text.query.all()
+
+    return text
+
+
+def update_text(text_id, **kwargs):
+    text = get_text(text_id)
+
+    for key, value in kwargs.items():
+        setattr(text, key, value)
+
+    db.session.commit()
+
+
+def delete_text(text_id):
+    text = get_text(text_id)
+
+    db.session.delete(text)
+    db.session.commit()
