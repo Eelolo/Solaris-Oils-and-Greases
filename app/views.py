9ba54c6371e1215a25a735bb10c4aad819695137
Eelolo <Eelolo@yandex.ru>
2021-05-29 23:54:37 +0300

    added lists to site pages

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, redirect
-from .models import Headers, Text, Tables, Pages
+from .models import Headers, Text, Tables, Pages, Lists
 from .crud.tables import load_table_content
+from .crud.lists import load_list_content
 
 
 main_bp = Blueprint('main', __name__)
@@ -19,13 +20,18 @@ def site_page(page_name):
     page_id = Pages.query.filter_by(name=page_name).one().id
     headers = Headers.query.filter_by(page_id=page_id).all()
     text = Text.query.filter_by(page_id=page_id).all()
+    lists = Lists.query.filter_by(page_id=page_id).all()
     tables = Tables.query.filter_by(page_id=page_id).all()
 
     for table in tables:
         content = load_table_content(table)
         table.content = content
 
-    elements = headers + text + tables
+    for list in lists:
+        content = load_list_content(list)
+        list.content = content
+
+    elements = headers + text + lists + tables
     elements.sort(key=lambda x: x.page_index)
 
     data = []
@@ -34,6 +40,8 @@ def site_page(page_name):
             data.append(('header', elem))
         if isinstance(elem, Text):
             data.append(('text', elem))
+        if isinstance(elem, Lists):
+            data.append(('list', elem))
         if isinstance(elem, Tables):
             data.append(('table', elem))
 
