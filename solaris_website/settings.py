dedf34fff17f1bfb3be3d6838ff98e9933f21648
Eelolo <Eelolo@yandex.ru>
2022-01-09 10:13:06 +0300

    test

--- solaris_website/settings.py
+++ solaris_website/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['89.179.124.46', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['89.179.124.46', 'eelolo-server', 'localhost', '127.0.0.1']
 
 
 # Application definition
